a6e80a9b27f3d505a3c7eaa66fcbb3fc16813b83
Ashinzekene <ashinzekene@gmail.com>
2018-06-15 09:02:01 +0100

    created service worker

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,13 @@
 const CACHE_NAME = 'cache_one';
-const CACHE_URLS = ['/src', '/src/index.html', '/src/styles/main.css', '/src/styles/main.js'];
+const CACHE_URLS = [
+  '/src',
+  '/src/images/ic_refresh_white_24px.svg',
+  '/src/index.html',
+  '/src/styles/main.css',
+  '/src/styles/main.js'
+];
+
+cache_request_url = 'https://movie-ease.herokuapp.com/api/movies/latest/1';
 
 self.addEventListener('install', e => {
   e.waitUntil(() => {
