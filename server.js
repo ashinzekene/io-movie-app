4b87920dfceb4e5ad6b2d22eb92808f6377c66d9
Ashinzekene <ashinzekene@gmail.com>
2018-06-16 08:21:00 +0100

    simple server

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-const express = require('express');
+var app,
+  server,
+  express = require('express'),
+  path = require('path'),
+  host = process.env.HOST || '127.0.0.1',
+  port = process.env.PORT || 3000,
+  root = path.resolve(__dirname, 'src');
 
-const app = express();
-
-app.get('/', (req, res) => {
-  res.sendFile('./src/index.html');
+app = express();
+app.use(function(req, res, next) {
+  console.log(req.url);
+  next();
 });
+app.use(express.static(root));
+server = app.listen(port, host, serverStarted);
+
+function serverStarted() {
+  console.log('Server started', host, port);
+  console.log('Root directory', root);
+  console.log('Press Ctrl+C to exit...\n');
+}
